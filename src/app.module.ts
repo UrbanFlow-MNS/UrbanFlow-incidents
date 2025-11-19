6cfca715dc2af89a4d014597053e19b165f341c1
LoganPal <logan.pallara@gmail.com>
2025-11-19 10:42:20 +0100

    changed the app.module.ts to config db (testing data rn)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IncidentEntity } from './models/entity/incident.entity';
+import { CategoryEntity } from './models/entity/category.entity';
+import { SiteEntity } from './models/entity/site.entity';
+import { InterventionEntity } from './models/entity/intervention.entity';
+import { AttachmentEntity } from './models/entity/attachment.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [
+        IncidentEntity,
+        CategoryEntity,
+        SiteEntity,
+        InterventionEntity,
+        AttachmentEntity,
+      ],
+      synchronize: true, // Enable on dev, disable on prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
