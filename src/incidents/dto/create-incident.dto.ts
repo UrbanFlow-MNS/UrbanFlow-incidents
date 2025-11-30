e2bff97ccca25078db3b3e3587b193ce2882fcb0
LoganPal <logan.pallara@gmail.com>
2025-11-30 17:38:18 +0100

    feature: deleted entities genereted, and begun CreateIncidentDto

--- src/incidents/dto/create-incident.dto.ts
+++ src/incidents/dto/create-incident.dto.ts
@@ -1,1 +1,36 @@
-export class CreateIncidentDto {}
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+
+export class CreateIncidentDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    description: string;
+
+    @IsString()
+    @IsNotEmpty()
+    createdBy: string;
+
+    @IsString()
+    @IsNotEmpty()
+    siteId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    categoryId: string;
+
+    @IsEnum()
+    @IsNotEmpty()
+    priority: Enumerator;
+
+    @IsEnum()
+    @IsNotEmpty()
+    status: Enumerator;
+
+}
