00d8ac6f218b254ed37f8a55eb4dc52edcad89a5
LoganPal <logan.pallara@gmail.com>
2025-12-03 13:03:20 +0100

    feature: added Site DTO + Type Date on create intervention DTO

--- src/interventions/dto/create-intervention.dto.ts
+++ src/interventions/dto/create-intervention.dto.ts
@@ -1,27 +1,37 @@
-import { IsNotEmpty } from "class-validator";
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateInterventionDto {
 
     @IsNotEmpty()
     incidentId: number;
 
+    @IsNumber()
+    @IsOptional()
     userId?: number;
 
+    @IsNumber()
     @IsNotEmpty()
     siteId: number;
 
-    @IsNotEmpty()
+    @IsDate()
+    @Type(() => Date)
     startAt: Date;
 
+    @IsDate()
+    @Type(() => Date)
     endAt?: Date;
 
+    @IsString()
+    @IsOptional()
     workNote?: string;
 
+    @IsNumber()
+    @IsOptional()
     travelTimes?: number;
 
+    @IsNumber()
+    @IsOptional()
     workTimes?: number;
-
-
-
     
 }
--- src/sites/dto/create-site.dto.ts
+++ src/sites/dto/create-site.dto.ts
@@ -1,1 +1,36 @@
-export class CreateSiteDto {}
+import { IsNotEmpty, IsNumberString, IsOptional, IsString } from "class-validator";
+
+export class CreateSiteDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    address: string;
+
+    @IsString()
+    @IsNotEmpty()
+    city: string;
+
+    @IsString()
+    @IsNotEmpty()
+    zipcode: string;
+
+    @IsNumberString() //Je ne sais pas si je dois mêttre un number, si ca pose problème, j'ai trouvé ca dans la doc
+    @IsNotEmpty()
+    latitude: number;
+
+    @IsNumberString() // Same 
+    @IsNotEmpty()
+    longitude: number;
+
+    @IsString()
+    @IsOptional()
+    contactName?: string;
+
+    @IsString()
+    @IsOptional()
+    contactPhone?: string;
+}
