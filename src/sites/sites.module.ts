6e180ec9edfa972a6d704d29e14c2caf167c46b3
LoganPal <logan.pallara@gmail.com>
2025-12-08 16:43:31 +0100

    feature: Added site service

--- src/sites/sites.module.ts
+++ src/sites/sites.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SitesService } from './sites.service';
 import { SitesController } from './sites.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SiteEntity } from '../models/entity/site.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SiteEntity])],
   controllers: [SitesController],
   providers: [SitesService],
 })
