fc1c7b5f363615c250982e7c1c72d56c1de59f8b
LoganPal <logan.pallara@gmail.com>
2026-03-30 05:39:51 +0200

    feat: expose TCP microservice and MessagePattern for gateway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: Number(process.env.TCP_PORT) || 6004,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(Number(process.env.API_PORT) || 3000);
 }
 bootstrap();
